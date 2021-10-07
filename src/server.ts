ad16c00b2d3b15f06ef0560deb380a4a4f6f8f33
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-08 00:27:58 +0700

    Install Express dan membuat server sederhana

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = 4000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
