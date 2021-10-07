ae7d8a10fafcd6e8085f73628af35ec755a0cf75
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-08 01:56:02 +0700

    Mempercantik tampilan terminal menggunakan Chalk

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import chalk from 'chalk';
 
 class App {
   public app: Application;
@@ -19,5 +20,7 @@ const port = 4000;
 const { app } = new App();
 
 app.listen(port, () => {
-  console.log("Server running on port " + port);
+  console.log(`Server started on port ${port}`);
+  console.log(chalk`Visit {rgb(128, 237, 153) http://localhost:${port}}`);
+  console.log(chalk`Developed by {rgb(255, 92, 88) Andry Pebrianto}`);
 });
